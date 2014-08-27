5c2cfba52e9d84d86b8d46db6f6f805c525c5e66
Cody <cody@UTARI-ROS-323.(none)>
2014-08-27 14:23:04 -0500

    Remaking node from scatch to aid in making tutorial

--- TurtlesimPositionController_pkg/src/PositionController_pubsub (copy).cpp
+++ TurtlesimPositionController_pkg/src/PositionController_pubsub (copy).cpp
@@ -1,23 +1,25 @@
 #include "ros/ros.h"
+#include <math.h>
 #include "geometry_msgs/Pose2D.h"										// to get desired position command
 #include "turtlesim/Pose.h"												// to read current position
 #include "turtlesim/Velocity.h"											// to send velocity command
 
-// Function declarations
 void ComPoseCallback(const geometry_msgs::Pose2D::ConstPtr& msg);
 void CurPoseCallback(const turtlesim::Pose::ConstPtr& msg);
+float GetErrorLin(turtlesim::Pose curpose, geometry_msgs::Pose2D despose);
+float GetErrorAng(turtlesim::Pose curpose, geometry_msgs::Pose2D despose);
 
-// Global variables
+
+geometry_msgs::Pose2D DesPose;											// variable to hold desired pose
 bool STOP = true;														// to hold stop flag, wait till first command given
 turtlesim::Velocity CmdVel;
 turtlesim::Pose CurPose;												// to hold current pose
-geometry_msgs::Pose2D DesPose;											// variable to hold desired pose
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "TurtlesimPositionController_pubsub");		// connect to roscore
-	ros::NodeHandle n;													// node object
+	ros::init(argc, argv, "LookatAroundModes_sub");						// connect to roscore
 	
+	ros::NodeHandle n;													// node object
 	// register sub to get desired position/pose commands
 	ros::Subscriber ComPose_sub = n.subscribe("/turtle1/PositionCommand", 5, ComPoseCallback);
 	// register sub to get current position/pose
@@ -26,6 +28,9 @@ int main(int argc, char **argv)
 	ros::Publisher Twist_pub = n.advertise<turtlesim::Velocity>("/turtle1/command_velocity", 100);
 	
 	ros::Rate loop_rate(10);											// freq to run loops in (10 Hz)
+	float ErrorLin = 0;
+	float ErrorAng = 0;
+	
 	
 	ROS_INFO("Ready to send position commands");						// let user know we are ready and good
 	while (ros::ok() && n.ok() )										// while ros and the node are ok
@@ -33,17 +38,29 @@ int main(int argc, char **argv)
 		ros::spinOnce();
 		if (STOP == false)												// and no stop command
 		{
-			printf("Processing...\n");
+			ErrorLin = GetErrorLin(CurPose, DesPose);
+			ErrorAng = GetErrorAng(CurPose, DesPose);
+			printf("Error linear: %f, Error angular: %f\n", ErrorLin, ErrorAng);
+			
+			if (ErrorLin > 0)
+			{
+				CmdVel.linear = 0.2 * ErrorLin;
+			}
+			else
+			{
+				CmdVel.linear = 0;
+			}
+			CmdVel.angular = 0.5 * ErrorAng;
 			Twist_pub.publish(CmdVel);
 		}
 		else
 		{
 			printf("Waiting...\n");
 		}
+		
 		loop_rate.sleep();												// sleep to maintain loop rate
 	}
 }
-
 
 // call back to send new desired Pose msgs
 void ComPoseCallback(const geometry_msgs::Pose2D::ConstPtr& msg)			
@@ -51,7 +68,7 @@ void ComPoseCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
 	STOP = false;														// start loop
 	DesPose.x = msg->x;													// copy msg to varible to use elsewhere
 	DesPose.y = msg->y;
-	CmdVel.angular += 1;
+	//~ DesPose.theta = msg->theta; // ignore theta, not using
 	return;
 }
 
@@ -62,4 +79,34 @@ void CurPoseCallback(const turtlesim::Pose::ConstPtr& msg)
 	CurPose.y = msg->y;
 	CurPose.theta = msg->theta;											// copy msg to varible to use elsewhere
 	return;
+}
+
+float GetErrorLin(turtlesim::Pose curpose, geometry_msgs::Pose2D despose)
+{
+	// create error vector
+	float Ex = despose.x - curpose.x;
+	float Ey = despose.y - curpose.y;
+	float Et = GetErrorAng(curpose, despose);
+	
+	// project error onto turtle x axis
+	//~ float Etx =  pow( pow(Ex,2.0) + pow(Ey,2.0), 0.5 )*cos(Et);
+	float Etx = hypotf(Ex, Ey)*cos(Et); // improved c function
+	
+	return Etx;
+}
+
+float GetErrorAng(turtlesim::Pose curpose, geometry_msgs::Pose2D despose)
+{
+	// create error vector
+	float Ex = despose.x - curpose.x;
+	float Ey = despose.y - curpose.y;
+	
+	// get desire angle
+	float dest = atan2f(Ey, Ex); // use float version to get arc tangent
+	
+	// get angle error
+	float Et = dest - curpose.theta;
+	
+	//~ ROS_INFO("Ex: %f, Ey: %f, Et: %f", Ex, Ey, Et);
+	return Et;
 }
